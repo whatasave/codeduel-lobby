5b9b9fc3afc538c155f76296c391c37218ad79fb
Michi Palazzo <palazzomichi@gmail.com>
2024-02-03 01:40:31 +0100

    Update

--- codeduel/client.go
+++ codeduel/client.go
@@ -18,6 +18,7 @@ const (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func StartWebSocket(response http.ResponseWriter, request *http.Request, lobby *Lobby, user *User) (*websocket.Conn, error) {
