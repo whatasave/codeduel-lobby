1989a7782b211aca6a4bb5dda03501de30efe31c
Michi Palazzo <palazzomichi@gmail.com>
2024-02-28 00:56:17 +0100

    Add runner

--- codeduel/runner.go
+++ codeduel/runner.go
@@ -1,5 +1,11 @@
 package codeduel
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
 type Runner struct {
 	url string
 }
@@ -14,7 +20,24 @@ func NewRunner(url string) Runner {
 	return Runner{url}
 }
 
-func (r *Runner) run(code string, input []string) []ExecutionResult {
-	// TODO
-	return []ExecutionResult{}
+func (r *Runner) Run(code string, input []string) ([]ExecutionResult, error) {
+	raw, _ := json.Marshal(struct {
+		Code  string   `json:"code"`
+		Input []string `json:"input"`
+	}{
+		Code:  code,
+		Input: input,
+	})
+	body := bytes.NewBuffer(raw)
+	response, err := http.Post(r.url+"/api/v1/run", "application/json", body)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	var result []ExecutionResult
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return []ExecutionResult{}, nil
 }
