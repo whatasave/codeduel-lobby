aab5b9f8f73d6606b45e49da4a39716c5ea095a9
Kaj Rusilowski <xedom08@gmail.com>
2025-02-08 23:00:21 +0100

    update

--- go.mod
+++ go.mod
@@ -6,11 +6,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
-	github.com/gorilla/websocket v1.5.1
+	github.com/gorilla/websocket v1.5.3
 	github.com/joho/godotenv v1.5.1
 )
 
-require (
-	github.com/felixge/httpsnoop v1.0.3 // indirect
-	golang.org/x/net v0.17.0 // indirect
-)
+require github.com/felixge/httpsnoop v1.0.3 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	"github.com/xedom/codeduel-lobby/codeduel"
 	"github.com/xedom/codeduel-lobby/codeduel/utils"
 )
@@ -13,12 +10,6 @@ var lobbies = make(map[string]*codeduel.Lobby)
 // var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
-	// loading env only if not in production
-	if utils.GetEnv("ENV", "development") != "production" {
-		if err := godotenv.Load(); err != nil {
-			log.Println("[MAIN] Error loading .env file")
-		}
-	}
 	config := utils.LoadConfig()
 	runner := codeduel.NewRunner(config.RunnerURL)
 	backend := codeduel.NewBackend(config.BackendURL, config.BackendApiKey)
